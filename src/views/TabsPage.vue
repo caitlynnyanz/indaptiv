967ff4547b4b1926ac2a4b4b0a9e7aa7772ba14f
Caitlynn Yanz <caitlynnyanz601@gmail.com>
2022-09-01 18:47:01 -0600

    created/outlined 5 main tabs for app

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -4,18 +4,28 @@
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+          <ion-icon :icon="home" />
+          <ion-label>Dashboard</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+          <ion-icon :icon="bulb" />
+          <ion-label>Habits</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="square" />
-          <ion-label>Tab 3</ion-label>
+          <ion-icon :icon="add" />
+          <ion-label>Create</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="tab2" href="/tabs/tab4">
+          <ion-icon :icon="document" />
+          <ion-label>Journal</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="tab2" href="/tabs/tab5">
+          <ion-icon :icon="calendar" />
+          <ion-label>Calendar</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
@@ -24,16 +34,18 @@
 
 <script>
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from "@ionic/vue";
-import { ellipse, square, triangle } from "ionicons/icons";
+import { home, bulb, add, document, calendar } from "ionicons/icons";
 
 export default {
   name: "TabsPage",
   components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage, IonRouterOutlet },
   setup() {
     return {
-      ellipse,
-      square,
-      triangle,
+      home,
+      bulb,
+      add,
+      document,
+      calendar,
     };
   },
 };
