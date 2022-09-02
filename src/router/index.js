6bc3abe0687636915bac9205a0662bbb58b301b0
Caitlynn Yanz <caitlynnyanz601@gmail.com>
2022-09-01 18:56:10 -0600

    installed axios, created and routed signup, login, and logout features

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from "@ionic/vue-router";
 
 import TabsPage from "../views/TabsPage.vue";
+import SignupView from "../views/SignupView.vue";
+import LoginView from "../views/LoginView.vue";
+import LogoutView from "../views/LogoutView.vue";
 
 const routes = [
   {
@@ -27,8 +30,19 @@ const routes = [
         path: "tab3",
         component: () => import("@/views/Tab3Page.vue"),
       },
+      {
+        path: "tab4",
+        component: () => import("@/views/Tab4Page.vue"),
+      },
+      {
+        path: "tab5",
+        component: () => import("@/views/Tab5Page.vue"),
+      },
     ],
   },
+  { path: "/signup", name: "signup", component: SignupView },
+  { path: "/login", name: "login", component: LoginView },
+  { path: "/logout", name: "logout", component: LogoutView },
 ];
 
 const router = createRouter({
